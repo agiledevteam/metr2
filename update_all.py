93008abdabfc4d2a77642aaae3ae5a4b87f4f45a
Jooyung Han <jooyung.han@gmail.com>
2014-02-27 10:41:50 +0900

    use test_client to update all repository

--- update_all.py
+++ update_all.py
@@ -1,7 +1,6 @@
 from metrapp import app
 from metrapp.views import *
 
-with app.app_context():
-  before_request()
-  update_repositories()
-  teardown_request()
+client = app.test_client()
+client.get('/updateall')
+
