b31bce17e6578846cdd82ffbc66216fbb294963d
Jooyung Han <jooyung.han@gmail.com>
2014-03-03 14:59:46 +0900

    updateall now calc delta

--- git.py
+++ git.py
@@ -34,7 +34,7 @@ class Git(object):
     return path.exists(self.gitdir)
 
   def fetch(self):
-    call(self.base_cmd + ['fetch'])
+    call(self.base_cmd + ['fetch', '--all', '-n'])
   
   def merge(self):
     call(self.base_cmd + ['merge', 'origin/' + self.branch])
@@ -133,6 +133,14 @@ def update(db, project_id):
     db.commit()
 
   metr_repository(git, already_processed, after_processing)
+
+  update_delta(db)
+
+def update_delta(db):
+  cur = db.execute('select c.id, c.sloc-p.sloc, c.floc-p.floc, c.codefat-p.codefat from commits c, commits p where c.parents = p.sha1')
+  for id, sloc, floc, codefat in cur.fetchall():
+    db.execute('update commits set delta_sloc=?, delta_floc=?, delta_codefat=? where id=?', [sloc,floc,codefat,id])
+
 
 def metr_repository(git, already_processed, after_processing):
   print len(cache)
