9b61481213eea2c65be35ed2d0fd8410cf17ce97
Jooyung Han <jooyung.han@gmail.com>
2014-03-14 19:10:04 +0900

    highlight projects (over average of codefat)

--- metrapp/static/js/controllers.js
+++ metrapp/static/js/controllers.js
@@ -7,7 +7,9 @@ app.controller('ProjectListCtrl', ['$scope', 'ProjectList', function($scope, Pro
   $scope.projects = ProjectList.projects;
   $scope.orderKey = 'codefat';
   $scope.reverseSort = true;
-  $scope.codefat = function() {
+  $scope.sloc = 0;
+  $scope.codefat = 0;
+  $scope.$watch('projects.length', function() {
     var sloc = 0;
     var floc = 0;
     for (var i=0, sz=$scope.projects.length; i<sz; i++) {
@@ -15,20 +17,13 @@ app.controller('ProjectListCtrl', ['$scope', 'ProjectList', function($scope, Pro
       sloc += p.sloc;
       floc += p.floc;
     }
+    $scope.sloc = sloc;
     if (sloc === 0) {
-      return 0;
+      $scope.codefat = 0;
     } else {
-      return 100 * floc/sloc;
+      $scope.codefat = 100 * floc/sloc;
     }
-  }
-  $scope.sloc = function() {
-    var sloc = 0;
-    for (var i=0, sz=$scope.projects.length; i<sz; i++) {
-      var p = $scope.projects[i];
-      sloc += p.sloc;
-    }
-    return sloc;
-  }
+  });
   $scope.sortBy = function(key) {
     if ($scope.orderKey === key) {
       $scope.reverseSort = !$scope.reverseSort;  
