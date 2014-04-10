b0918886f45fb5dbc2657e1c54f823ea395f0fd7
Jooyung Han <jooyung.han@gmail.com>
2014-04-10 18:06:44 +0900

    add user activities calendar graph(in progress)

--- metrapp/static/js/directives.js
+++ metrapp/static/js/directives.js
@@ -68,6 +68,81 @@ app.directive('inlinePie', function() {
 	}
 	return {
 		restrict: 'A',
+		link: link
+	};
+});
+
+app.directive('calendarGraph', function() {
+	function link(scope, element, attr) {
+		var x = 11;
+		var y = 11;
+		var days = 365;
+		var dx = 2;
+		var dy = 2;
+
+		var day = new Date();
+		day.setDate(day.getDate() - days - 1);
+		day.setDate(day.getDate() - day.getDay());
+
+		var weeks = d3.range(Math.floor((days + 1) / 7));
+		console.log(weeks);
+		var days = d3.range(7);
+
+		var calendar = d3.select(element[0])
+				.attr('class', 'calendar-graph')
+				.style('display', 'inline-block')
+				.attr({width: '721px', height: '110px'})
+			.append('div').append('svg')
+				.attr({width: '721px', height: '110px'})
+			.append('g')
+				.attr('transform', 'translate(20,20)');
+
+		var legend = d3.select(element[0]).append('div').attr('class', 'activity-legend');
+		legend.append('span').text("Less");
+		legend.append('ul').attr('class', 'legend')
+			.selectAll('li')
+			.data(['#eee', '#d6e685', '#8cc665', '#44a340', '#1e6823'])
+			.enter().append('li').style('background-color', function(d){return d;});
+		legend.append('span').text("More");
+
+		var dayOfWeekLabel = [{title:'S', style:{display:'none'}}
+			,{title:'M', style:{}}
+			,{title:'T', style:{display:'none'}}
+			,{title:'W', style:{}}
+			,{title:'T', style:{display:'none'}}
+			,{title:'F', style:{}}
+			,{title:'S', style:{display:'none'}}];
+		calendar
+			.selectAll('g')
+			.data(weeks).enter().append('g')
+				.attr('transform', function(d, i) {
+					return 'translate(' + (i*(x + dx))+ ')';
+				}).selectAll('rect')
+					.data(days).enter().append('rect')
+						.style('fill', '#eee')
+						.attr({width: x, height: y})
+						.attr("y", function(d,i){
+							return i*(y + dy);
+						});
+		calendar.selectAll('text.wday')
+			.data(dayOfWeekLabel).enter().append('text').attr('class', 'wday')
+			.text(function(d){ return d.title;})
+			.style("display", function(d){
+				return d.style.display;
+			})
+			.attr('text-anchor', 'middle')
+			.attr('dx', -10)
+			.attr('dy', function(d,i){ return 9 + (y+dy)*i;});
+
+		var week = 0;
+		for (var i=0; i<366; i++) {
+
+			day.setDate(day.getDate() + 1);
+			if (day.getDay() == 6)
+				week++;
+		}
+	}
+	return {
 		link: link,
 	};
 });
